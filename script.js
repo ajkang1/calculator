f54fb59b0863d334fb0323c2608b893b2c85c4a2
akang1 <ajkang110@gmail.com>
2024-03-07 11:43:52 -0800

    add event listeners to operators and conditionals to prevent consecutive operators

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
-let display = document.querySelector('display');
-let numbers = document.querySelectorAll(".num");
+const display = document.querySelector('.display');
+const numbers = document.querySelectorAll('.num');
+const operators = document.querySelectorAll('.op');
+const compute = document.querySelector('.compute');
 
 numbers.forEach((num) => {
     num.addEventListener('click', () => {
-        display.textContent = "test";
+        display.textContent += num.innerHTML;
     })
 })
+
+operators.forEach((op) => {
+    op.addEventListener('click', () => {
+        if(display.textContent.substring(display.textContent.length - 1) !== op.innerHTML){
+            display.textContent += op.innerHTML;
+        }
+    });
+});
+
+const miscs = document.querySelectorAll('.misc');
+miscs.forEach((misc) => {
+    misc.addEventListener('click', () => {
+        if(misc.innerHTML == "AC"){
+            display.textContent = "";
+        }else if(misc.innerHTML == "+/-"){
+            display.textContent = "-" + display.textContent;
+        }else{
+            // finish
+        }
+    });
+});
+
+function calculate(){
+    
+}
