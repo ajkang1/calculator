0977397adc6e1a059aff432ac246e982c4c7b4cd
akang1 <ajkang110@gmail.com>
2024-03-10 16:25:16 -0700

    added event listeners to all buttons need to fix bug in calculate function

--- script.js
+++ script.js
@@ -1,35 +1,108 @@
-const display = document.querySelector('.display');
+let op = "";
+let num1 = "";
+let num2 = "";
+const display = document.querySelector(".display");
 const numbers = document.querySelectorAll('.num');
-const operators = document.querySelectorAll('.op');
 const compute = document.querySelector('.compute');
+const operators = document.querySelectorAll('.op');
+const miscs = document.querySelectorAll('.misc');
+const decimal = document.querySelector('.decimal');
 
 numbers.forEach((num) => {
     num.addEventListener('click', () => {
-        display.textContent += num.innerHTML;
-    })
-})
-
-operators.forEach((op) => {
-    op.addEventListener('click', () => {
-        if(display.textContent.substring(display.textContent.length - 1) !== op.innerHTML){
-            display.textContent += op.innerHTML;
+        if(op === "" || num1 === ""){
+            num1 += num.innerHTML;
+            console.log(num1);
+            display.textContent = num1;
+        }else{
+            num2 += num.innerHTML;
+            console.log(num2);
+            display.textContent = num2;
         }
     });
 });
 
-const miscs = document.querySelectorAll('.misc');
 miscs.forEach((misc) => {
     misc.addEventListener('click', () => {
-        if(misc.innerHTML == "AC"){
-            display.textContent = "";
-        }else if(misc.innerHTML == "+/-"){
-            display.textContent = "-" + display.textContent;
-        }else{
-            // finish
+        switch(misc.innerHTML){
+            case "AC":
+                clear();
+                break;
+            case "+/-":
+                if(op === "" || num1 === ""){
+                    num1 *= -1;
+                    display.textContent = num1;
+                }else{
+                    num2 *= -1;
+                    display.textContent = num2;
+                }
+                break;
+            case "%":
+                if(op === "" || num1 === ""){
+                    num1 /= 100;
+                    display.textContent = num1;
+                }else{
+                    num2 /= 100;
+                    display.textContent = num2;
+                }
+                break;
         }
     });
 });
 
-function calculate(){
-    
+operators.forEach((operator) => {
+    operator.addEventListener('click', () => {
+        if(op !== "" && num2 !== ""){
+            calculate(num1, op, num2);
+            num1 = display.textContent;
+            op = operator.innerHTML;
+        }else{
+            op = operator.innerHTML;
+        }
+        console.log(op);
+    });
+});
+
+decimal.addEventListener('click', () => {
+    if(display.textContent.indexOf('.') === -1){
+        if(op === "" || num1 === ""){
+            num1 += ".";
+            display.textContent = num1;
+        }else{
+            num2 += ".";
+            display.textContent = num2;
+        }
+    }
+});
+
+compute.addEventListener('click', () => {
+    calculate(num1, op, num2);
+});
+
+function clear(){
+    num1 = "";
+    num2 = "";
+    op = "";
+    display.textContent = "";
+}
+
+function calculate(numOne, operator, numTwo){
+    switch(operator){
+        case "+":
+            display.textContent = +numOne + +numTwo;
+            break;
+        case "-":
+            display.textContent = +numOne - +numTwo;
+            break;
+        case "*":
+            display.textContent = +numOne * +numTwo;
+            break;
+        case "/":
+            display.textContent = +numOne / +numTwo;
+            break;
+    }
+    num1 = display.textContent; //sets value of num1 to be ans and clears the rest
+    num2 = "";
+    op = "";
+    console.log(num1);
 }
